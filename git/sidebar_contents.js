c632cbe451d8bf0fd0e94626a4d6d36eb15b0440
Ryanjiena <ryanjiena@foxmail.com>
2020-05-24 18:16:28 +0800

    Create sidebar_contents.js

---
+++ git/sidebar_contents.js
@@ -0,0 +1,18 @@
+module.exports = [
+  //   "/git/",
+  "git.md",
+  "01_Getting_Started.md",
+  "02_Installing_Git.md"",
+  "03_Git_Configuration.md",
+  "04_Git_Basics.md",
+  "05_Git_Branching.md",
+  "06_Git_Tags.md",
+  "07_Git_Graphical_Tools.md",
+  "08_Github.md",
+  "09_Gitee.md",
+  "10_Coding.md",
+  "11_Docker_Gitlab.md",
+];
+
+
+
